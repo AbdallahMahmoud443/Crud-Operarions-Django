3e82fd94ae64e8c0cf1634827902971607adc17c
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2024-07-20 00:00:38 +0300

    sort and search done

--- PayRollApp/views.py
+++ PayRollApp/views.py
@@ -5,7 +5,7 @@ from PayRollApp.models import Employee  # type: ignore
 from django.shortcuts import redirect # type: ignore
 from django.core.paginator import Paginator,PageNotAnInteger # type: ignore
 from django.conf import settings # type: ignore
-
+from django.db.models import Q
 
 def EmployeeList(request):
     # employees = Employee.objects.all() # To Get All Record In Table of Employee
@@ -61,7 +61,35 @@ def AddEmployee(request):
 def PageWiseEmployeesList(request):
     page_size = int(request.GET.get('page_size',getattr(settings,'PAGE_SIZE',3))) # PAGE_SIZE =>variable in settings file or 5 by default
     page = request.GET.get('page',1)
-    Employees = Employee.objects.select_related('EmpDepartment','EmpCountry').all()
+    search_query = request.GET.get('search','')
+    # this query related with sort functionality
+    '''
+    #! Wrong Code For Sorting
+    # this to query parameter is required for sortable libarary of sortable at same syntax => (sort_by,sort_order)
+    sort_by = request.GET.get('sort_by','id') # => columns
+    sort_order=request.GET.get('sort_order','asc') # => ordering
+    
+    # validation on keyword of sorting (columns)
+    valid_sort_fields = ['id','FirstName','Salary','Hire Data']
+    if sort_by not in valid_sort_fields :
+       sort_by = 'id'
+    '''
+    
+    # 1-this code is related with search functionality 
+    Employees = Employee.objects.filter(
+                                        Q(id__icontains=search_query) |
+                                        Q(FirstName__icontains=search_query) |
+                                        Q(LastName__icontains=search_query) |
+                                        Q(Salary__icontains=search_query))
+    '''
+    #! Wrong Code For Sorting 
+    # 2-Apply Soring Code (ordering)
+    if sort_order == 'desc':
+       Employees.order_by(f'-{sort_by}') # nigative sign is denote desc ordering 
+    else:
+       Employees.order_by(sort_by)
+    '''
+    #Employees = Employee.objects.select_related('EmpDepartment','EmpCountry').all()
     paginator = Paginator(Employees,page_size) 
     try:
         employees_page =paginator.page(page)
@@ -69,5 +97,8 @@ def PageWiseEmployeesList(request):
         employees_page =paginator.page(1)
     return render(request,'PayRollApp/WiseEmployeesList.html',{
         'Employees_Page':employees_page,
-        'page_size':page_size
+        'page_size':page_size,
+        'search_query':search_query,
+        #'sort_by':sort_by,
+        #'sort_order':sort_order
     })
